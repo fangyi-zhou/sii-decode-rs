96b36488fa04ff504d269e42563bc8055506958e
Fangyi Zhou <me@fangyi.io>
2024-08-15 23:09:04 +0100

    Parse more types of values

--- src/bsii_decode.rs
+++ src/bsii_decode.rs
@@ -6,10 +6,10 @@ use std::collections::HashMap;
 use std::str;
 
 use nom::bytes::complete::{tag, take};
-use nom::combinator::{fail, peek};
+use nom::combinator::{fail, map};
 use nom::multi::{count, many_till};
-use nom::number::complete::{le_u32, le_u64};
-use nom::sequence::{pair, preceded};
+use nom::number::complete::{le_f32, le_i32, le_i64, le_u16, le_u32, le_u64, le_u8};
+use nom::sequence::{pair, tuple};
 use nom::IResult;
 
 use log::info;
@@ -37,8 +37,49 @@ struct ValuePrototype<'a> {
 
 struct DataBlock<'a> {
     type_id: u32,
-    id: u64,
-    data: Vec<&'a [u8]>, // TODO: Decode values
+    id: Id,
+    data: Vec<DataValue<'a>>,
+}
+
+#[derive(PartialEq, Debug)]
+enum Id {
+    Nameless(u64),
+    Named(Vec<String>),
+}
+
+#[derive(PartialEq, Debug)]
+enum DataValue<'a> {
+    String(&'a str),
+    StringArray(Vec<&'a str>),
+    EncodedString(String),
+    EncodedStringArray(Vec<String>),
+    Float(f32),
+    FloatArray(Vec<f32>),
+    FloatVec2((f32, f32)),
+    FloatVec3((f32, f32, f32)),
+    FloatVec3Array(Vec<(f32, f32, f32)>),
+    Int32Vec3((i32, i32, i32)),
+    Int32Vec3Array(Vec<(i32, i32, i32)>),
+    FloatVec4((f32, f32, f32, f32)),
+    FloatVec4Array(Vec<(f32, f32, f32, f32)>),
+    // Float Vec 7 for version 1 not supported
+    FloatVec8((f32, f32, f32, f32, f32, f32, f32, f32)),
+    FloatVec8Array(Vec<(f32, f32, f32, f32, f32, f32, f32, f32)>),
+    Int32(i32),
+    Int32Array(Vec<i32>),
+    UInt32(u32),
+    UInt32Array(Vec<u32>),
+    UInt16(u16),
+    UInt16Array(Vec<u16>),
+    Int64(i64),
+    Int64Array(Vec<i64>),
+    UInt64(u64),
+    UInt64Array(Vec<u64>),
+    Bool(bool),
+    BoolArray(Vec<bool>),
+    Enum(u32),
+    Id(Id),
+    IdArray(Vec<Id>),
 }
 
 impl<'a> BsiiFile<'a> {
@@ -51,6 +92,11 @@ impl<'a> BsiiFile<'a> {
 fn bsii_parser(input: &[u8]) -> IResult<&[u8], BsiiFile<'_>> {
     let (input, header) = tag("BSII")(input)?;
     let (input, version) = le_u32(input)?;
+    if version == 1 {
+        // Doesn't support version 1
+        // It has differing behaviour with value type 0x19
+        return fail(input);
+    }
     let mut prototypes = HashMap::new();
     let mut data_blocks = Vec::new();
     // TODO: Rewrite the loop using combinators
@@ -73,14 +119,19 @@ fn bsii_parser(input: &[u8]) -> IResult<&[u8], BsiiFile<'_>> {
                     },
                 ));
             } else {
-                let (next_input, prototype) = prototype_parse(loop_input)?;
+                let (next_input, prototype) = prototype_parser(loop_input)?;
                 info!("Parsed prototype {}", prototype.name);
                 prototypes.insert(prototype.id, prototype);
                 loop_input = next_input;
             }
         } else {
-            let (next_input, data_block) = data_block_parse(loop_input, &prototypes)?;
-            info!("Parsed data block {:X}", data_block.id);
+            let (next_input, data_block) = data_block_parser(loop_input, &prototypes)?;
+            // TODO: Use Display instead of Debug
+            info!(
+                "Parsed data block with prototype {}, ID {:?}",
+                prototypes.get(&data_block.type_id).unwrap().name,
+                data_block.id
+            );
             data_blocks.push(data_block);
             loop_input = next_input;
         }
@@ -91,6 +142,23 @@ fn str_parser(input: &[u8]) -> IResult<&[u8], &str> {
     let (input, len) = le_u32(input)?;
     let (input, data) = take(len)(input)?;
     Ok((input, str::from_utf8(data).unwrap()))
+}
+
+const CHAR_ENCODINGS: [char; 37] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_',
+];
+
+fn encoded_str_parser(input: &[u8]) -> IResult<&[u8], String> {
+    let (input, encoded_data) = le_u64(input)?;
+    let mut remaining = encoded_data & !(1u64 << 63);
+    let mut chars: Vec<char> = Vec::new();
+    while remaining > 0 {
+        let last = remaining % 38;
+        remaining /= 38;
+        chars.push(CHAR_ENCODINGS[last as usize - 1]);
+    }
+    Ok((input, chars.into_iter().collect()))
 }
 
 fn value_prototype_parser(input: &[u8]) -> IResult<&[u8], ValuePrototype<'_>> {
@@ -121,7 +189,7 @@ fn value_prototype_parser(input: &[u8]) -> IResult<&[u8], ValuePrototype<'_>> {
     }
 }
 
-fn prototype_parse(input: &[u8]) -> IResult<&[u8], Prototype<'_>> {
+fn prototype_parser(input: &[u8]) -> IResult<&[u8], Prototype<'_>> {
     let (input, _) = tag("\0\0\0\0")(input)?;
     let (input, _) = tag(&[0x01])(input)?;
     let (input, id) = le_u32(input)?;
@@ -137,32 +205,174 @@ fn prototype_parse(input: &[u8]) -> IResult<&[u8], Prototype<'_>> {
     ))
 }
 
-fn value_parse(input: &[u8], type_id: u32) -> IResult<&[u8], &[u8]> {
-    // TODO: Parse into values instead of u8 slices
-    match type_id {
-        0x05u32 => {
-            // single
-            take(4usize)(input)
-        }
-        0x25u32 => {
-            // int32
-            take(4usize)(input)
-        }
-        0x34u32 => {
-            // array of uint64
-            let (input, size) = peek(le_u32)(input)?;
-            take(size as usize * 8 + 4usize)(input)
-        }
-        0x36u32 => {
-            // array of ByteBool
-            let (input, size) = peek(le_u32)(input)?;
-            take(size as usize + 4usize)(input)
-        }
-        _ => todo!("Implement value parse for type_id {}", type_id),
+fn id_parser(input: &[u8]) -> IResult<&[u8], Id> {
+    let (input, length) = le_u8(input)?;
+    if length == 0xff {
+        let (input, nameless_id) = le_u64(input)?;
+        Ok((input, Id::Nameless(nameless_id)))
+    } else {
+        let (input, parts) = count(encoded_str_parser, length as usize)(input)?;
+        Ok((input, Id::Named(parts)))
     }
 }
 
-fn data_block_parse<'a, 'b>(
+fn value_parser(input: &[u8], type_id: u32) -> IResult<&[u8], DataValue<'_>> {
+    match type_id {
+        0x01u32 => {
+            // string
+            map(str_parser, DataValue::String)(input)
+        }
+        0x02u32 => {
+            // array of string
+            let (input, size) = le_u32(input)?;
+            map(count(str_parser, size as usize), DataValue::StringArray)(input)
+        }
+        0x03u32 => {
+            // encoded string
+            map(encoded_str_parser, DataValue::EncodedString)(input)
+        }
+        0x04u32 => {
+            // array of encoded string
+            let (input, size) = le_u32(input)?;
+            map(
+                count(encoded_str_parser, size as usize),
+                DataValue::EncodedStringArray,
+            )(input)
+        }
+        0x05u32 => {
+            // float
+            map(le_f32, DataValue::Float)(input)
+        }
+        0x06u32 => {
+            // array of float
+            let (input, size) = le_u32(input)?;
+            map(count(le_f32, size as usize), DataValue::FloatArray)(input)
+        }
+        0x07u32 => {
+            // vec2 of float
+            map(pair(le_f32, le_f32), DataValue::FloatVec2)(input)
+        }
+        0x09u32 => {
+            // vec3 of float
+            map(tuple((le_f32, le_f32, le_f32)), DataValue::FloatVec3)(input)
+        }
+        0x11u32 => {
+            // vec3 of int32
+            map(tuple((le_i32, le_i32, le_i32)), DataValue::Int32Vec3)(input)
+        }
+        0x12u32 => {
+            // array of vec3 of int32
+            let (input, size) = le_u32(input)?;
+            map(
+                count(tuple((le_i32, le_i32, le_i32)), size as usize),
+                DataValue::Int32Vec3Array,
+            )(input)
+        }
+        0x18u32 => {
+            // array of vec4 of float
+            let (input, size) = le_u32(input)?;
+            map(
+                count(tuple((le_f32, le_f32, le_f32, le_f32)), size as usize),
+                DataValue::FloatVec4Array,
+            )(input)
+        }
+        0x19u32 => {
+            // vec8 of float
+            map(
+                tuple((
+                    le_f32, le_f32, le_f32, le_f32, le_f32, le_f32, le_f32, le_f32,
+                )),
+                DataValue::FloatVec8,
+            )(input)
+        }
+        0x1au32 => {
+            // array of vec8 of float
+            let (input, size) = le_u32(input)?;
+            map(
+                count(
+                    tuple((
+                        le_f32, le_f32, le_f32, le_f32, le_f32, le_f32, le_f32, le_f32,
+                    )),
+                    size as usize,
+                ),
+                DataValue::FloatVec8Array,
+            )(input)
+        }
+        0x25u32 => {
+            // int32
+            map(le_i32, DataValue::Int32)(input)
+        }
+        0x26u32 => {
+            // array of int32
+            let (input, size) = le_u32(input)?;
+            map(count(le_i32, size as usize), DataValue::Int32Array)(input)
+        }
+        0x27u32 | 0x2fu32 => {
+            // uint32
+            map(le_u32, DataValue::UInt32)(input)
+        }
+        0x28u32 => {
+            // array of uint32
+            let (input, size) = le_u32(input)?;
+            map(count(le_u32, size as usize), DataValue::UInt32Array)(input)
+        }
+        0x2bu32 => {
+            // uint16
+            map(le_u16, DataValue::UInt16)(input)
+        }
+        0x2cu32 => {
+            // array of uint16
+            let (input, size) = le_u32(input)?;
+            map(count(le_u16, size as usize), DataValue::UInt16Array)(input)
+        }
+        0x31u32 => {
+            // int64
+            map(le_i64, DataValue::Int64)(input)
+        }
+        0x32u32 => {
+            // array of int64
+            let (input, size) = le_u32(input)?;
+            map(count(le_i64, size as usize), DataValue::Int64Array)(input)
+        }
+        0x33u32 => {
+            // uint64
+            map(le_u64, DataValue::UInt64)(input)
+        }
+        0x34u32 => {
+            // array of uint64
+            let (input, size) = le_u32(input)?;
+            map(count(le_u64, size as usize), DataValue::UInt64Array)(input)
+        }
+        0x35u32 => {
+            // bool
+            map(le_u8, |v| DataValue::Bool(v != 0))(input)
+        }
+        0x36u32 => {
+            // array of bool
+            let (input, size) = le_u32(input)?;
+            map(
+                count(map(le_u8, |v| v != 0), size as usize),
+                DataValue::BoolArray,
+            )(input)
+        }
+        0x37u32 => {
+            // enum
+            map(le_u32, DataValue::Enum)(input)
+        }
+        0x39u32 | 0x3bu32 | 0x3du32 => {
+            // id
+            map(id_parser, DataValue::Id)(input)
+        }
+        0x3au32 | 0x3cu32 => {
+            // array of id
+            let (input, length) = le_u32(input)?;
+            map(count(id_parser, length as usize), DataValue::IdArray)(input)
+        }
+        _ => todo!("Implement value parse for type_id {:x}", type_id),
+    }
+}
+
+fn data_block_parser<'a, 'b>(
     input: &'a [u8],
     prototypes: &'b HashMap<u32, Prototype<'b>>,
 ) -> IResult<&'a [u8], DataBlock<'a>> {
@@ -172,12 +382,12 @@ fn data_block_parse<'a, 'b>(
         fail(input)
     } else {
         let prototype = prototypes.get(&type_id).unwrap();
-        let (input, id) = preceded(tag(&[0xff]), le_u64)(input)?;
+        let (input, id) = id_parser(input)?;
         // TODO: Try to rewrite the code below in combinators
-        let mut data: Vec<&'a [u8]> = Vec::new();
+        let mut data: Vec<DataValue<'a>> = Vec::new();
         let mut loop_input = input;
         for value in &prototype.value_prototypes {
-            let (next_input, value) = value_parse(loop_input, value.type_id)?;
+            let (next_input, value) = value_parser(loop_input, value.type_id)?;
             loop_input = next_input;
             data.push(value);
         }
@@ -244,7 +454,7 @@ mod tests {
             0x72, 0x72, 0x61, 0x79, 0x5F, 0x66, 0x69, 0x65, 0x6C, 0x64, // value name
             0x00, 0x00, 0x00, 0x00, // value type
         ];
-        match prototype_parse(test_prototype) {
+        match prototype_parser(test_prototype) {
             Ok((input, prototype)) => {
                 assert_eq!(input, &[]);
                 assert_eq!(prototype.id, 0x01u32);
@@ -269,7 +479,7 @@ mod tests {
             0x64, // value name
             0x00, 0x00, 0x00, 0x00, // value type
         ];
-        match prototype_parse(test_prototype) {
+        match prototype_parser(test_prototype) {
             Ok((input, prototype)) => {
                 assert_eq!(input, &[]);
                 assert_eq!(prototype.id, 0x02u32);
@@ -312,18 +522,18 @@ mod tests {
             0x00, 0x01, 0x00, // array of ByteBool
             0x00, 0x00, 0x00, 0x00, // length of the following array
         ];
-        match data_block_parse(test_data_block, &prototypes) {
+        match data_block_parser(test_data_block, &prototypes) {
             Ok((input, data_block)) => {
                 assert_eq!(input, &[]);
                 assert_eq!(data_block.type_id, 1);
-                assert_eq!(data_block.id, 0x0807060504030201u64);
+                assert_eq!(data_block.id, Id::Nameless(0x0807060504030201u64));
                 assert_eq!(data_block.data.len(), 3);
-                assert_eq!(data_block.data[0], &[0xff, 0xff, 0xff, 0xff]);
+                assert_eq!(data_block.data[0], DataValue::Int32(-1));
                 assert_eq!(
                     data_block.data[1],
-                    &[0x03, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00]
+                    DataValue::BoolArray(vec![false, true, false])
                 );
-                assert_eq!(data_block.data[2], &[0x00, 0x00, 0x00, 0x00]);
+                assert_eq!(data_block.data[2], DataValue::UInt64Array(vec![]));
             }
             Err(err) => panic!("Failed to parse, {}", err),
         }
@@ -346,13 +556,13 @@ mod tests {
             0xFF, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF, // block ID
             0x00, 0x00, 0x80, 0x3F, // single value
         ];
-        match data_block_parse(test_data_block, &prototypes) {
+        match data_block_parser(test_data_block, &prototypes) {
             Ok((input, data_block)) => {
                 assert_eq!(input, &[]);
                 assert_eq!(data_block.type_id, 2);
-                assert_eq!(data_block.id, 0xfffefdfcfbfaf9f8u64);
+                assert_eq!(data_block.id, Id::Nameless(0xfffefdfcfbfaf9f8u64));
                 assert_eq!(data_block.data.len(), 1);
-                assert_eq!(data_block.data[0], &[0x00, 0x00, 0x80, 0x3f]);
+                assert_eq!(data_block.data[0], DataValue::Float(1.0f32));
             }
             Err(err) => panic!("Failed to parse, {}", err),
         }
