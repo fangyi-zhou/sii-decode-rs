34889e4de48326f69ce4c66ed0e6a131b4aae2af
Fangyi Zhou <me@fangyi.io>
2025-05-08 18:09:31 +0100

    Allow tests to run in browsers

--- tests/wasm.rs
+++ tests/wasm.rs
@@ -1,6 +1,8 @@
 use sii_decode::wasm::decode;
 use wasm_bindgen_test::wasm_bindgen_test;
 
+wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
 #[wasm_bindgen_test]
 fn test_decode_with_Siin() {
     let input = b"SiiN";
