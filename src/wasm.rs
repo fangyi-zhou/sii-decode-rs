9a7241c4c1488f46505613b2c475f91ecae1271a
Fangyi Zhou <me@fangyi.io>
2025-05-08 17:56:18 +0100

    Export WebAssembly decode API

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,7 +1,12 @@
-use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::{prelude::wasm_bindgen, JsError};
+
+use crate::file_type::decode_until_siin;
 
 #[wasm_bindgen]
-pub fn parse(input: &[u8]) -> String {
-    // TODO
-    "".to_string()
+pub fn decode(input: &[u8]) -> Result<String, JsError> {
+    if let Some(result) = decode_until_siin(input) {
+        Ok(String::from_utf8(result).unwrap())
+    } else {
+        Err(JsError::new("Failed to process input"))
+    }
 }
