871dddf1fa9f10d8ba71fa1c0c05d9c09cc981dc
Fangyi Zhou <me@fangyi.io>
2025-05-08 17:38:51 +0100

    Implement a function to decode until complete

--- src/file_type.rs
+++ src/file_type.rs
@@ -1,5 +1,9 @@
 //! Handles file types for SII files.
 
+use log::info;
+
+use crate::{bsii_file::BsiiFile, scsc_file::ScscFile};
+
 /// FileType enum representing different file types.
 #[derive(Debug, PartialEq, Eq)]
 pub enum FileType {
@@ -25,6 +29,32 @@ pub fn detect_file_type(file_content: &[u8]) -> Option<FileType> {
         Some(FileType::Siin)
     } else {
         None
+    }
+}
+
+/// Given a supported file, decode until the textual SII format is reached.
+pub fn decode_until_siin(file_content: &[u8]) -> Option<Vec<u8>> {
+    let content = file_content;
+    let file_type = detect_file_type(file_content)?;
+    info!("Obtained file type: {:?}", file_type);
+    match file_type {
+        FileType::Scsc => {
+            let scsc_file = ScscFile::parse(content).unwrap();
+            let decoded_content = scsc_file.decode();
+            match detect_file_type(&decoded_content)? {
+                FileType::Siin => Some(decoded_content),
+                FileType::Bsii => {
+                    let bsii_file = BsiiFile::parse(&decoded_content).unwrap();
+                    Some(bsii_file.to_siin().as_bytes().to_vec())
+                }
+                _ => unreachable!("Unexpected file type after decoding"),
+            }
+        }
+        FileType::Bsii => {
+            let bsii_file = BsiiFile::parse(content).unwrap();
+            Some(bsii_file.to_siin().into())
+        }
+        FileType::Siin => Some(content.to_vec()),
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::env;
 use std::fs;
 
-use bsii_file::BsiiFile;
-use scsc_file::ScscFile;
 use simple_logger::SimpleLogger;
 
 mod bsii_file;
 mod bsii_output;
 mod bsii_parse;
+mod file_type;
 mod scsc_file;
 mod scsc_parse;
 
@@ -19,9 +18,6 @@ fn main() {
 
     let arg = env::args().next_back().unwrap();
     let content = fs::read(arg).unwrap();
-    let scsc_file = ScscFile::parse(content.as_slice()).unwrap();
-    let bsii_binary = scsc_file.decode();
-    fs::write("output.bsii", &bsii_binary).unwrap();
-    let bsii_file = BsiiFile::parse(&bsii_binary).unwrap();
-    println!("{}", bsii_file.to_siin());
+    let decoded = file_type::decode_until_siin(&content).unwrap();
+    println!("{}", String::from_utf8(decoded).unwrap());
 }
