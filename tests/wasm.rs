95100a46527d86a0adb86c532e8abf1634e448aa
Fangyi Zhou <me@fangyi.io>
2025-05-08 17:56:43 +0100

    Add a test for WebAssembly API

---
+++ tests/wasm.rs
@@ -0,0 +1,9 @@
+use sii_decode::wasm::decode;
+use wasm_bindgen_test::wasm_bindgen_test;
+
+#[wasm_bindgen_test]
+fn test_decode_with_Siin() {
+    let input = b"SiiN";
+    let result = decode(input).unwrap();
+    assert_eq!(result, "SiiN");
+}
