8b2d9bd804b3f52d8e5b449a657e0b219cd94934
Fangyi Zhou <me@fangyi.io>
2025-05-11 18:13:52 +0100

    Test failure case for decoding in wasm

--- tests/wasm.rs
+++ tests/wasm.rs
@@ -4,8 +4,16 @@ use wasm_bindgen_test::wasm_bindgen_test;
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn test_decode_with_Siin() {
+fn test_decode_with_siin() {
+    // Given a SiiN file, the decoding should return the same content.
     let input = b"SiiN";
     let result = decode(input).unwrap();
     assert_eq!(result, "SiiN");
+}
+
+#[wasm_bindgen_test]
+fn test_decode_with_failure() {
+    // Given an invalid file, the decoding should return an error.
+    let input = b"Invalid data";
+    decode(input).expect_err("Decoding error: Unknown file type");
 }
