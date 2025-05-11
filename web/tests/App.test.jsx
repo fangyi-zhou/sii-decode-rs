656d60d40be55b238534e5953840970a58dbd8b2
Fangyi Zhou <me@fangyi.io>
2025-05-11 19:48:11 +0100

    ???

--- web/tests/App.test.jsx
+++ web/tests/App.test.jsx
@@ -5,45 +5,53 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 test("App can render", async () => {
-  render(<App />);
-  // Has a file upload box
-  expect(screen.getByTestId("file-upload")).toBeInTheDocument();
+    expect(true).toBe(false);
+//   render(<App />);
+//   // Has a file upload box
+//   expect(screen.getByTestId("file-upload")).toBeInTheDocument();
 
-  // Has a download button
-  const downloadButton = screen.getByTestId("file-download");
-  expect(downloadButton).toBeInTheDocument();
-  expect(downloadButton).toHaveAttribute("href", "#");
+//   // Has a download button
+//   const downloadButton = screen.getByTestId("file-download");
+//   expect(downloadButton).toBeInTheDocument();
+//   expect(downloadButton).toHaveAttribute("href", "#");
 
-  // Has a read only text area for displaying
-  const textArea = screen.getByTestId("file-display");
-  expect(textArea).toBeInTheDocument();
-  expect(textArea).toHaveAttribute("readonly");
-  // Initially empty
-  expect(textArea).toHaveValue("");
+//   // Has a read only text area for displaying
+//   const textArea = screen.getByTestId("file-display");
+//   expect(textArea).toBeInTheDocument();
+//   expect(textArea).toHaveAttribute("readonly");
+//   // Initially empty
+//   expect(textArea).toHaveValue("");
 });
 
-test("App can decode", async () => {
-  render(<App />);
-  const fileUploadBox = screen.getByTestId("file-upload");
-  const file = new File(["SiiN"], "test.sii");
-  userEvent.upload(fileUploadBox, file);
+// test("App can decode", async () => {
+//   render(<App />);
+//   const fileUploadBox = screen.getByTestId("file-upload");
+//   const file = new File(["SiiN"], "test.sii");
+//   userEvent.upload(fileUploadBox, file);
 
-  const textArea = screen.getByTestId("file-display");
-  // Wait for the text area to be updated
-  await waitFor(() => {
-    expect(textArea).toHaveValue("SiiN");
-  });
-});
+//   const textArea = screen.getByTestId("file-display");
+//   // Wait for the text area to be updated
+//   await waitFor(() => {
+//     expect(textArea).toHaveValue("SiiN");
 
-test("App can display error", async () => {
-  render(<App />);
-  const fileUploadBox = screen.getByTestId("file-upload");
-  const file = new File(["invalid"], "test.sii");
-  userEvent.upload(fileUploadBox, file);
+//     const downloadButton = screen.getByTestId("file-download");
+//     expect(downloadButton).not.toHaveAttribute("href", "#");
 
-  const textArea = screen.getByTestId("file-display");
-  // Wait for the text area to be updated
-  await waitFor(() => {
-    expect(textArea).toHaveDisplayValue(/Error:/);
-  });
-});
+//   });
+// });
+
+// test("App can display error", async () => {
+//   render(<App />);
+//   const fileUploadBox = screen.getByTestId("file-upload");
+//   const file = new File(["invalid"], "test.sii");
+//   userEvent.upload(fileUploadBox, file);
+
+//   const textArea = screen.getByTestId("file-display");
+//   // Wait for the text area to be updated
+//   await waitFor(() => {
+//     expect(textArea).toHaveDisplayValue(/Error:/);
+
+//     const downloadButton = screen.getByTestId("file-download");
+//     expect(downloadButton).toHaveAttribute("href", "#");
+//   });
+// });
