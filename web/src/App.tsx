52626eb73440f02356398cbf0210ddd130b20025
Fangyi Zhou <me@fangyi.io>
2025-05-11 18:30:30 +0100

    web: Add basic error handling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,6 +10,17 @@ function App() {
   const handleFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const selectedFile = event.target.files[0];
+      if (textAreaRef.current) {
+        // Clear the text area
+        textAreaRef.current.value = "";
+      }
+      if (downloadRef.current) {
+        // Clear the download link
+        if (downloadRef.current.href !== "#") {
+          URL.revokeObjectURL(downloadRef.current.href);
+        }
+        downloadRef.current.href = "#";
+      }
       setFile(selectedFile);
     }
   };
@@ -20,19 +31,24 @@ function App() {
       reader.onload = (e) => {
         const arrayBuffer = e.target?.result as ArrayBuffer;
         const bytes = new Uint8Array(arrayBuffer);
-        const decoded = decode(bytes);
-        if (textAreaRef.current) {
-          textAreaRef.current.value = decoded;
-        }
-        if (downloadRef.current) {
-          const blob = new Blob([decoded], { type: "text/plain" });
-          const url = URL.createObjectURL(blob);
-          downloadRef.current.href = url;
-          downloadRef.current.download = file.name.replace(
-            ".sii",
-            "-decoded.sii",
-          );
-          downloadRef.current.textContent = "Download";
+        try {
+          const decoded = decode(bytes);
+          if (textAreaRef.current) {
+            textAreaRef.current.value = decoded;
+          }
+          if (downloadRef.current) {
+            const blob = new Blob([decoded], { type: "text/plain" });
+            const url = URL.createObjectURL(blob);
+            downloadRef.current.href = url;
+            downloadRef.current.download = file.name.replace(
+              ".sii",
+              "-decoded.sii",
+            );
+          }
+        } catch (error) {
+          if (textAreaRef.current && error instanceof Error) {
+            textAreaRef.current.value = error.toString();
+          }
         }
       };
       reader.readAsArrayBuffer(file);
