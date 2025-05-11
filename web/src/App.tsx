101cc76cd70424f6d72e31699c7d6baa1556a463
Fangyi Zhou <me@fangyi.io>
2025-05-11 22:21:39 +0100

    web: Minor tweaks

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import { decode } from "sii-decode-rs";
 
@@ -7,52 +7,54 @@ function App() {
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const downloadRef = useRef<HTMLAnchorElement>(null);
 
-  const handleFile = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      const selectedFile = event.target.files[0];
-      if (textAreaRef.current) {
-        // Clear the text area
-        textAreaRef.current.value = "";
-      }
-      if (downloadRef.current) {
-        // Clear the download link
-        if (downloadRef.current.href !== "#") {
-          URL.revokeObjectURL(downloadRef.current.href);
-        }
-        downloadRef.current.href = "#";
-      }
-      setFile(selectedFile);
+  const handleFile = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files || event.target.files.length === 0) {
+      return;
     }
-  };
+    const selectedFile = event.target.files[0];
+    if (textAreaRef.current) {
+      // Clear the text area
+      textAreaRef.current.value = "";
+    }
+    if (downloadRef.current) {
+      // Clear the download link
+      if (downloadRef.current.href !== "#") {
+        URL.revokeObjectURL(downloadRef.current.href);
+      }
+      downloadRef.current.href = "#";
+    }
+    setFile(selectedFile);
+  }, [setFile, textAreaRef, downloadRef]);
 
   useEffect(() => {
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const arrayBuffer = e.target?.result as ArrayBuffer;
-        const bytes = new Uint8Array(arrayBuffer);
-        try {
-          const decoded = decode(bytes);
-          if (textAreaRef.current) {
-            textAreaRef.current.value = decoded;
-          }
-          if (downloadRef.current) {
-            const blob = new Blob([decoded], { type: "text/plain" });
-            const url = URL.createObjectURL(blob);
-            downloadRef.current.href = url;
-            downloadRef.current.download = file.name.replace(
-              ".sii",
-              "-decoded.sii",
-            );
-          }
-        } catch (error) {
-          if (textAreaRef.current && error instanceof Error) {
-            textAreaRef.current.value = error.toString();
-          }
-        }
-      };
-      reader.readAsArrayBuffer(file);
+    if (!file) {
+      return;
     }
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const arrayBuffer = e.target?.result as ArrayBuffer;
+      const bytes = new Uint8Array(arrayBuffer);
+      try {
+        const decoded = decode(bytes);
+        if (textAreaRef.current) {
+          textAreaRef.current.value = decoded;
+        }
+        if (downloadRef.current) {
+          const blob = new Blob([decoded], { type: "text/plain" });
+          const url = URL.createObjectURL(blob);
+          downloadRef.current.href = url;
+          downloadRef.current.download = file.name.replace(
+            ".sii",
+            "-decoded.sii",
+          );
+        }
+      } catch (error) {
+        if (textAreaRef.current && error instanceof Error) {
+          textAreaRef.current.value = error.toString();
+        }
+      }
+    };
+    reader.readAsArrayBuffer(file);
   }, [file]);
 
   return (
@@ -81,9 +83,17 @@ function App() {
           Download
         </a>
       </div>
-      <p className="read-the-docs">
-        Your file will not be uploaded to the server. It will be decoded in your
+      <p className="footer">
+        Your file is not uploaded to any server, it is decoded using your own
         browser.
+        <br />
+        This tools is{" "}
+        <a href="https://github.com/fangyi-zhou/sii-decode-rs/">open source</a>.
+        If you encounter any issues, please report them{" "}
+        <a href="https://github.com/fangyi-zhou/sii-decode-rs/issues">
+          on GitHub
+        </a>
+        .
       </p>
     </>
   );
