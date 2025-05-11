1e90ae947262996eb7af9a05d556833b3459d5a2
Fangyi Zhou <me@fangyi.io>
2025-05-11 22:51:45 +0100

    UI and documentation tweaks (#14)
    * web: Minor tweaks
    
    * web: More minor tweaks
    
    * doc: Update README, add HACKING.md
    
    * web: Fix README ordered list

--- README.md
+++ README.md
@@ -1,24 +1,40 @@
-# A Rust library to decode SII save files
+# A Rust library to decode SII files
 
 This library provides functionalities to decode SII files ("Unit serialized
 file") that are used in SCS Software games (e.g. Euro Truck Simulator 2).
 
-This library is ported from existing SII decrypt software by František
-Milt https://github.com/TheLazyTomcat/SII_Decrypt and Joshua Menzel
-https://gitlab.com/jammerxd/sii-decryptsharp.
-Many thanks for the initial investigation and the implementation work.
+This library is ported from existing SII decrypt software by [František
+Milt (SII_Decrypt)](https://github.com/TheLazyTomcat/SII_Decrypt) and [Joshua
+Menzel (sii-decryptsharp)](https://gitlab.com/jammerxd/sii-decryptsharp).
+Many thanks for their investigation and implementation work.
 
-This library is also available in a binary executable tool that takes a file and
-outputs the decrypted/decoded result.
+This library is also available in an executable format that takes a file of an
+supported format and outputs the decrypted/decoded result.
 
 ## Usage
 
-TODO
+You can use the web interface at https://sii-decode.github.io/
+
+## Technical Notes
+
+The `src` directory contains the source code of the Rust library. The library
+handles 3 types of files, identified by their header types: scsc (encrypted,
+compressed data files), bsii (binary data files), and siin (textual data types).
+
+The Rust library is then compiled into Web Assembly using `wasm-pack`, so that
+the library can be used in browsers. The `web` directory contains the source
+code of the web interface. This allows the decoding work to be performed in the
+browser, so that users can use the tool in their browser without the need to
+upload their files to a server.
+
+## Contributing
+
+See [HACKING.md](./HACKING.md).
 
 ## Roadmap
 
 - [ ] Implement a proper CLI for the binary tool.
 - [ ] Add unit tests for serialising BSII files
 - [ ] Define the WebAssembly API
-- [ ] Provide the tool as a web application (using WebAssembly)
+- [X] Provide the tool as a web application (using WebAssembly)
 - [ ] Use the decode result to make an achievement tracker tool
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import { decode } from "sii-decode-rs";
 
@@ -7,12 +7,15 @@ function App() {
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const downloadRef = useRef<HTMLAnchorElement>(null);
 
-  const handleFile = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
+  const handleFile = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (!event.target.files || event.target.files.length === 0) {
+        return;
+      }
       const selectedFile = event.target.files[0];
       if (textAreaRef.current) {
         // Clear the text area
-        textAreaRef.current.value = "";
+        textAreaRef.current.value = "Decoding...";
       }
       if (downloadRef.current) {
         // Clear the download link
@@ -22,43 +25,45 @@ function App() {
         downloadRef.current.href = "#";
       }
       setFile(selectedFile);
-    }
-  };
+    },
+    [setFile, textAreaRef, downloadRef],
+  );
 
   useEffect(() => {
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const arrayBuffer = e.target?.result as ArrayBuffer;
-        const bytes = new Uint8Array(arrayBuffer);
-        try {
-          const decoded = decode(bytes);
-          if (textAreaRef.current) {
-            textAreaRef.current.value = decoded;
-          }
-          if (downloadRef.current) {
-            const blob = new Blob([decoded], { type: "text/plain" });
-            const url = URL.createObjectURL(blob);
-            downloadRef.current.href = url;
-            downloadRef.current.download = file.name.replace(
-              ".sii",
-              "-decoded.sii",
-            );
-          }
-        } catch (error) {
-          if (textAreaRef.current && error instanceof Error) {
-            textAreaRef.current.value = error.toString();
-          }
-        }
-      };
-      reader.readAsArrayBuffer(file);
+    if (!file) {
+      return;
     }
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const arrayBuffer = e.target?.result as ArrayBuffer;
+      const bytes = new Uint8Array(arrayBuffer);
+      try {
+        const decoded = decode(bytes);
+        if (textAreaRef.current) {
+          textAreaRef.current.value = decoded;
+        }
+        if (downloadRef.current) {
+          const blob = new Blob([decoded], { type: "text/plain" });
+          const url = URL.createObjectURL(blob);
+          downloadRef.current.href = url;
+          downloadRef.current.download = file.name.replace(
+            ".sii",
+            "-decoded.sii",
+          );
+        }
+      } catch (error) {
+        if (textAreaRef.current && error instanceof Error) {
+          textAreaRef.current.value = error.toString();
+        }
+      }
+    };
+    reader.readAsArrayBuffer(file);
   }, [file]);
 
   return (
     <>
-      <h1>SII Decode (beta)</h1>
-      <p>Select your file</p>
+      <h1>SII Decode</h1>
+      <p>Select a SII file to decode</p>
       <div>
         <input
           type="file"
@@ -74,16 +79,25 @@ function App() {
         cols={50}
         ref={textAreaRef}
         data-testid="file-display"
+        spellCheck="false"
         readOnly
       />
       <div>
         <a href="#" ref={downloadRef} data-testid="file-download">
-          Download
+          Download decoded file
         </a>
       </div>
-      <p className="read-the-docs">
-        Your file will not be uploaded to the server. It will be decoded in your
+      <p className="footer">
+        Your file is not uploaded to any server, it is decoded using your own
         browser.
+        <br />
+        This tools is{" "}
+        <a href="https://github.com/fangyi-zhou/sii-decode-rs/">open source</a>.
+        If you encounter any issues, please report them{" "}
+        <a href="https://github.com/fangyi-zhou/sii-decode-rs/issues">
+          on GitHub
+        </a>
+        .
       </p>
     </>
   );
